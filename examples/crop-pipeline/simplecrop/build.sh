9b841e4e9a73fc50fec7bbf6bc109734e6a7f2bc
Calvin Pritchard <pritchard.calvin@gmail.com>
2021-03-18 23:34:01 -0700

    refactor: delete unused files

--- examples/crop-pipeline/simplecrop/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o nounset
-set -o pipefail
-set -o errexit
-
-docker build -t openmodelingfoundation/simplecrop -f Dockerfile ..
--- examples/crop-pipeline/simplecrop/check_installed/check_installed.py
+++
@@ -1,6 +0,0 @@
-import simplecrop_cli_python
-import shutil
-
-simplecrop_cli_python.run('../../simplecrop/target/cli/simplecrop', '.')
-# shutil.rmtree('output')
-# shutil.rmtree('data')
