6adc8d9a13b9e825b0062ae7825695430ab89c25
Calvin Pritchard <pritchard.calvin@gmail.com>
2021-04-08 20:14:56 -0700

    docs: added docstrings for overland flow methods

--- examples/crop-pipeline/overlandflow/model.py
+++ examples/crop-pipeline/overlandflow/model.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
-from typing import Any, Dict
-
-from landlab import RasterModelGrid
 from landlab.components.overland_flow import OverlandFlow
 from landlab.components import SoilInfiltrationGreenAmpt
 from meillionen.meillionen import FuncInterface
-import numpy as np
+from meillionen.io import LandLabLoader, PandasLoader, NetCDF4Saver
 import pandas as pd
 import xarray as xr
 
-from meillionen.io import LandLabLoader, PandasLoader, NetCDF4Saver
 
 interface = FuncInterface.from_dict({
     "name": "overlandflow",
@@ -55,10 +51,13 @@ interface = FuncInterface.from_dict({
 
 def run_day(mg, precipitation: float, duration=1800):
     """
+    Calculates the mm of water that has infiltrated into soil after a rainfall event.
+    Rainfall events are assumed to be instantaneous and constant over the model grid.
+
     :param dem: model grid
-    :param precipitation: rainfall in m/hr
+    :param precipitation: rainfall in mm/hr
     :param duration: seconds of rainfall
-    :return:
+    :return: mm of water infiltrated into the soil at every coordinate in the grid
     """
     elapsed_time = 0.0
     swd = mg.add_zeros('surface_water__depth', at='node', clobber=True)
@@ -77,13 +76,24 @@ def run_day(mg, precipitation: float, duration=1800):
     return xr.DataArray(mg.at_node['soil_water_infiltration__depth'].reshape(mg.shape), dims=('x', 'y'))
 
 
-def run_year(mg, weather, swid):
+def run_year(mg, weather: pd.DataFrame, swid):
+    """
+    Calculates the mm of water that has infiltrated each grid cell for day of the year after a rainfall event
+
+    :param mg: model grid
+    :param weather: total rainfall in mm each day of a year
+    :param swid: reference to data cube with x, y, time dimensions used to incrementally save model results
+    """
     for t, row in weather.iterrows():
         infiltration_depth = run_day(mg, row.rainfall__depth)
         swid.set_slice(infiltration_depth, time=t)
 
 
-if __name__ == '__main__':
+def run_year_cli():
+    """
+    Calculates the mm of water that has infiltrated each grid cell for day of the year after a rainfall event
+    in response to a request on the command line
+    """
     args = interface.to_cli()
     weather = args.get_source('weather')
     elevation = args.get_source('elevation')
@@ -92,6 +102,7 @@ if __name__ == '__main__':
     mg = LandLabLoader.load(elevation)
     weather = PandasLoader.load(weather)
     swid_schema = interface.to_dict()['sinks']['soil_water_infiltration__depth']['data_type']['dimensions']
+    # create dimension label / size parts [('x', 10), ('y', 10), ('time', 365')]
     swid_schema = list(zip(swid_schema, [mg.shape[0], mg.shape[1], weather.shape[0]]))
     with NetCDF4Saver(surface_water_infiltation__depth, swid_schema) as swid:
         run_year(
@@ -99,3 +110,7 @@ if __name__ == '__main__':
             weather=weather,
             swid=swid,
         )
+
+
+if __name__ == '__main__':
+    run_year_cli()
