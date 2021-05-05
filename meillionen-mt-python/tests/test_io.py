516b3b3ea1db914be3fa1003a8eefe1c4abc3504
Calvin Pritchard <pritchard.calvin@gmail.com>
2021-05-04 22:25:33 -0700

    fix: tests in python meillionen wrapper

--- meillionen-mt-python/tests/test_io.py
+++ meillionen-mt-python/tests/test_io.py
@@ -1,13 +1,13 @@
 import netCDF4
 import numpy as np
-from meillionen.resource import PandasLoaderSaver, FeatherResource, NetCDFResource, NetCDFPreSaver, Unvalidated
+from meillionen.resource import PandasHandler, FeatherResource, NetCDFResource, NetCDFHandler, Unvalidated
 import pytest
 import xarray as xr
 
 
 def test_load_feather():
     source = FeatherResource.from_dict({"path": "../examples/crop-pipeline/simplecrop/data/yearly.feather"})
-    df = PandasLoaderSaver(Unvalidated()).load(source)
+    df = PandasHandler(Unvalidated()).load(source)
     assert df.shape == (1, 26)
 
 
@@ -18,7 +18,7 @@ def test_save_netcdf():
         dimensions=['x', 'y', 'time']
     )
     swid_schema = [('x', 6), ('y', 11), ('time', 365)]
-    with NetCDFPreSaver(Unvalidated()).load(sink, swid_schema) as swid:
+    with NetCDFHandler(Unvalidated()).save(sink, swid_schema) as swid:
         xs = xr.DataArray(np.array(range(6*11)).reshape((6, 11)), dims=('x', 'y'))
         swid.set_slice(xs, time=5)
     with netCDF4.Dataset('data/swid.nc', 'r') as swid:
