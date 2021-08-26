e1c7b529f6d6ff2faf6ad5a0957478e6d85efc80
Calvin Pritchard <pritchard.calvin@gmail.com>
2021-08-26 14:27:54 -0700

    feat: roughed in app class cli replacement

--- examples/crop-pipeline/overlandflow/model.py
+++ examples/crop-pipeline/overlandflow/model.py
@@ -1,39 +1,69 @@
 #!/usr/bin/env python3
 from landlab.components.overland_flow import OverlandFlow
 from landlab.components import SoilInfiltrationGreenAmpt
-from meillionen.meillionen import server_respond_from_cli
-from meillionen.handlers import LandLabGridHandler, PandasHandler, NetCDFSliceHandler
-from meillionen.function import FuncInterfaceServer, FuncRequest
+from meillionen.server import App
+from meillionen.interface.method_interface import MethodInterface
+from meillionen.interface.class_interface import ClassInterface
+from meillionen.interface.module_interface import ModuleInterface
+from meillionen.interface.schema import PandasHandler, LandLabGridHandler, NetCDFSliceHandler
+import pyarrow as pa
 import pandas as pd
 import xarray as xr
 
-model = FuncInterfaceServer(
-    sources={
-        'weather': PandasHandler.from_kwargs(
-            description='Daily weather data for a year',
-            columns={
-                'fields': [
-                    {
-                        "name": "rainfall__depth",
-                        "data_type": "Float64"
-                    }
-                ]
-            }
+
+settings = PandasHandler(
+    name='settings',
+    s=pa.schema([
+        pa.field(name='soil_type', type=pa.string(), bool_nullable=False)
+    ])
+)
+
+
+run = MethodInterface(
+    name='run',
+    sources=[
+        PandasHandler(
+            name='weather',
+            s=pa.schema([
+                pa.field(name='rainfall__depth', type=pa.float64(), bool_nullable=False)
+            ])
         ),
-        'elevation': LandLabGridHandler.from_kwargs(
-            description='Elevation model. Each cell is a sq m',
-            data_type='Float64',
-            dimensions=['x', 'y']
-        )
-    },
-    sinks={
-        'soil_water_infiltration__depth': NetCDFSliceHandler.from_kwargs(
-            description='Surface water depth at each point in grid for each day in year',
-            data_type='Float64',
+        LandLabGridHandler(
+            name='elevation',
+            data_type='f8'
+        ),
+        settings
+    ],
+    sinks=[
+        NetCDFSliceHandler(
+            name='soil_water_infiltration__depth',
+            data_type='f8',
             dimensions=['x', 'y', 'time']
         )
-    })
+    ],
+    handler=run_year_handler
+)
 
+
+default_settings = MethodInterface(
+    name='default_settings',
+    sinks=[
+        settings
+    ]
+)
+
+
+overlandflow_module = ModuleInterface(
+    classes=[
+        ClassInterface(
+            name='simplecrop',
+            methods=[
+                run,
+                default_settings
+            ]
+        )
+    ]
+)
 
 # external metadata file with more detailed information
 
@@ -78,28 +108,34 @@ def run_year(mg, weather: pd.DataFrame, swid):
         swid.set({'time': t}, infiltration_depth)
 
 
-def run_year_cli():
+def run_year_handler(sinks, sources):
     """
     Calculates the mm of water that has infiltrated each grid cell for day of the year after a rainfall event
     in response to a request on the command line
     """
-    rb = server_respond_from_cli('overlandflow', model.to_recordbatch('overlandflow'))
-    print(rb.to_pandas()[['resource', 'payload']])
-    args = FuncRequest.from_recordbatch(rb)
-    weather = args.source('weather')
-    elevation = args.source('elevation')
-    surface_water_infiltation__depth = args.sink('soil_water_infiltration__depth')
 
-    model_grid = model.source('elevation').load(elevation)
-    weather = model.source('weather').load(weather)
-    swid_size = {'x': model_grid.shape[0], 'y': model_grid.shape[1], 'time': weather.shape[0]}
-    with model.sink('soil_water_infiltration__depth').save(surface_water_infiltation__depth, swid_size) as swid:
+    # sinks need to be written to manually by their handlers since we need to decide what data to write
+    surface_water_infiltation__depth = sinks['surface_water_infiltation__depth']
+
+    # sources are already processed by their handlers
+    elevation = sources['elevation']
+    weather = sources['weather']
+
+    # determine the size of the spatial daily infiltration array
+    swid_size = {'x': elevation.shape[0], 'y': elevation.shape[1], 'time': weather.shape[0]}
+
+    # write to the spatial daily infiltration netcdf array
+    with surface_water_infiltation__depth.save(swid_size) as swid:
         run_year(
-            mg=model_grid,
+            mg=elevation,
             weather=weather,
             swid=swid,
         )
 
 
+app = App(overlandflow_module)
+
+
 if __name__ == '__main__':
-    run_year_cli()
+    # handles cli request, prints help or handles requests using some other communication method
+    app.run()
