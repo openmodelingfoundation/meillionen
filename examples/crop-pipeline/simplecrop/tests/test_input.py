9f7ff05d325ac2fc9233279d04aa4bcda7081e7f
Calvin Pritchard <pritchard.calvin@gmail.com>
2021-10-06 15:03:09 -0700

    test: added run test for simplecrop

---
+++ examples/crop-pipeline/simplecrop/tests/test_input.py
@@ -0,0 +1,46 @@
+from meillionen.interface.method_request import MethodRequest
+from meillionen.interface.resource import Feather, OtherFile
+from simplecrop_omf import server
+from simplecrop_omf.io import \
+    _write_irrig_fwf,\
+    _write_plant_fwf,\
+    _write_simctrl_fwf,\
+    _write_soil_fwf,\
+    _write_weather_fwf, \
+    run_one_year
+import pandas as pd
+from meillionen.resource import FeatherResource, FileResource
+
+
+def test_write_yearly():
+    yearly = pd.read_feather('data/yearly.feather')
+    _write_plant_fwf('plant.inp', yearly)
+    _write_simctrl_fwf('simctrl.inp', yearly)
+    _write_soil_fwf('soil.inp', yearly)
+
+
+def test_write_daily():
+    daily = pd.read_feather('data/daily.feather')
+    _write_irrig_fwf('irrig.inp', daily)
+    _write_weather_fwf('weather.inp', daily)
+
+
+def test_run_one_year():
+    sources = {
+        'daily': Feather('data/daily.feather'),
+        'yearly': Feather('data/yearly.feather')
+    }
+    sinks = {
+        'plant': Feather('output/plant.feather'),
+        'soil': Feather('output/soil.feather'),
+        'raw': OtherFile('output/raw')
+    }
+    mr = MethodRequest(
+        class_name='simplecrop',
+        method_name='run',
+        kwargs={
+            **sources,
+            **sinks
+        }
+    )
+    server.run(mr)
